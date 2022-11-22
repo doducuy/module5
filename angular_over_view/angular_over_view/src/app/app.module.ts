53d433db81d205b80e3e8588012f9a159670254c
doducuy <doducuyktdn@gmail.com>
2022-11-22 13:42:31 +0700

    nop bai

--- angular_over_view/angular_over_view/src/app/app.module.ts
+++ angular_over_view/angular_over_view/src/app/app.module.ts
@@ -2,14 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
 import { Ss3ColorPickerComponent } from './ss3-color-picker/ss3-color-picker.component';
 import { Ss3CalculatorComponent } from './ss3-calculator/ss3-calculator.component';
+import { HomeComponent } from './case-study/home/home.component';
+import { FacilityComponent } from './case-study/facility/facility.component';
+import {FormsModule} from '@angular/forms';
+import { CreateHouseComponent } from './case-study/facility/create/create-house/create-house.component';
+import { CreateRoomComponent } from './case-study/facility/create/create-room/create-room.component';
+import { CreateVillaComponent } from './case-study/facility/create/create-villa/create-villa.component';
+import { CustomerListComponent } from './case-study/customer/customer-list/customer-list.component';
+import { CreateComponent } from './case-study/customer/create/create.component';
+import { HeaderComponent } from './case-study/header/header.component';
+import { FooterComponent } from './case-study/footer/footer.component';
+import { EditHouseComponent } from './case-study/facility/edit/edit-house/edit-house.component';
+import { EditRoomComponent } from './case-study/facility/edit/edit-room/edit-room.component';
+import { EditVillaComponent } from './case-study/facility/edit/edit-villa/edit-villa.component';
+import { EditCustomerComponent } from './case-study/customer/edit-customer/edit-customer.component';
 @NgModule({
   declarations: [
     AppComponent,
     Ss3ColorPickerComponent,
     Ss3CalculatorComponent,
+    HomeComponent,
+    FacilityComponent,
+    CreateHouseComponent,
+    CreateRoomComponent,
+    CreateVillaComponent,
+    CustomerListComponent,
+    CreateComponent,
+    HeaderComponent,
+    FooterComponent,
+    EditHouseComponent,
+    EditRoomComponent,
+    EditVillaComponent,
+    EditCustomerComponent,
   ],
   imports: [
     BrowserModule,
